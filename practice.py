3e267210708aa854a8eeb89c1c1488cf7c80cac4
italianroses <rose818@gmail.com>
2022-08-31 21:32:25 +0900

    t#

---
+++ practice.py
@@ -0,0 +1,23 @@
+# 애완동물을 소개해 주세요
+
+animal = "고양이"
+
+name = "해피"
+
+age = 4
+
+hobby = "낮잠"
+
+is_adult = age >= 3
+
+'''여러문장 
+주석처리
+과정'''
+
+'''드래그 후 comand + /'''
+
+print("우리집 ", animal, " 의 이름은 ", name, "예요")
+print(name + " 은 ", age, " 이며, ", hobby, " 을 아주 좋아해요")
+print(name + " 는 어른 일까요? ", is_adult,)
+
+
