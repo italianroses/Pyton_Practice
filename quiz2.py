1ea0367a095071e46b376d24155ecb58adc43433
italianroses <112529060+italianroses@users.noreply.github.com>
2022-09-01 10:29:42 +0900

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,8 @@
+from random import *
+
+date = randint(4, 28)
+
+print("오프라인 스터디 모임 날짜는 매월 ",date , " 일로 선정되었습니다.")
+
+
+# answer : ,date , == + str(date) +
---
+++ randomfuction.py
@@ -0,0 +1,14 @@
+from random import *
+
+# print(random()) # 0.0 ~1.0 미만의 임의의 값 생성
+# print(random()*10) # 0.0 ~10.0 미만의 임의의 값 생성
+# print(round(random()*10))
+# print(int(random()*10)) # int 정수값
+# print(int(random()*10)+1) # 1.0 ~10.0 미만의 임의의 값 생성
+
+# 복권번호
+print(int(random()*45+1)) # 1~45 이하의 값을 생성
+
+print(randrange(1, 46)) # 1~46 미만의 임의의 값 생성
+print(randint(1, 45)) # 1~45 미만의 임의의 값 생성
+
