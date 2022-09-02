19c9ff5c9ae0ce6becd9f9e66494988153832ea9
italianroses <112529060+italianroses@users.noreply.github.com>
2022-09-02 10:24:42 +0900

    Update randomfuction.py

--- randomfuction.py
+++ randomfuction.py
@@ -10,5 +10,5 @@ from random import *
 print(int(random()*45+1)) # 1~45 이하의 값을 생성
 
 print(randrange(1, 46)) # 1~46 미만의 임의의 값 생성
-print(randint(1, 45)) # 1~45 미만의 임의의 값 생성
+print(randint(1, 45)) # 1~45 ㅇㅣㅎㅏ의 임의의 값 생성
 
